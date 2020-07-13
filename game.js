f0f0dc616a7a3f77b55a880dd16d9e74819d9677
faranak Sabet <faranaksabet@yahoo.com>
2020-07-12 20:46:37 -0700

    my Third commit

--- game.js
+++ game.js
@@ -42,16 +42,42 @@ const MAX_QUESTIONS = 3;
 startGame = () => {
   questionCounter = 0;
   score = 0;
-  availableQuestios = [...questions];
-  console.log(availableQuestions);
+  availableQuestions = [...questions];
+  //console.log(availableQuestions);
   getNewQuestion();
 };
 
 getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    return window.location.assign("/end.html");
+  }
   questionCounter++;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
 };
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectAnswer = selectedChoice.dataset["number"];
+
+    const classToApply = selectAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+    console.log(classToApply);
+
+    getNewQuestion();
+  });
+});
 
 startGame();
